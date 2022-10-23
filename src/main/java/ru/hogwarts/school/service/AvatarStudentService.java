404a1c517cd32334a3437a693ba94b4cfd2a40df
NatShiv <happy_fox@bk.ru>
2022-10-24 01:58:19 +0300

    3.6 test

--- src/main/java/ru/hogwarts/school/service/AvatarStudentService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarStudentService.java
@@ -1,17 +1,21 @@
 package ru.hogwarts.school.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.entity.AvatarStudent;
 import ru.hogwarts.school.entity.Student;
 import ru.hogwarts.school.exception.AvatarNotFoundException;
+import ru.hogwarts.school.exception.DataEntryError;
 import ru.hogwarts.school.repository.AvatarStudentRepository;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Optional;
 
 import static java.nio.file.StandardOpenOption.CREATE_NEW;
@@ -28,35 +32,35 @@ public class AvatarStudentService {
         this.studentService = studentService;
     }
 
-     public void uploadAvatarStudent(Long id, MultipartFile file) {
+    public void uploadAvatarStudent(Long id, MultipartFile file) {
         Student student = studentService.findStudent(id);
-         String extension = Optional.ofNullable(file.getOriginalFilename()).
-                 map(s -> s.substring(file.getOriginalFilename().lastIndexOf("."))).
-                 orElse(" ");
-         Path filePath = Path.of(avatarsDir, id +  extension);
+        String extension = Optional.ofNullable(file.getOriginalFilename()).
+                map(s -> s.substring(file.getOriginalFilename().lastIndexOf("."))).
+                orElse(" ");
+        Path filePath = Path.of(avatarsDir, id + extension);
         try {
             Files.createDirectories(filePath.getParent());
             Files.deleteIfExists(filePath);
         } catch (IOException ex) {
             throw new RuntimeException("Возникла проблема при создании файла или директории.");
         }
-         try (InputStream inputStream = file.getInputStream();
-              OutputStream outputStream = Files.newOutputStream(filePath, CREATE_NEW);
-              BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, 1024);
-              BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream, 1024)) {
-             bufferedInputStream.transferTo(bufferedOutputStream);
-         } catch (IOException e) {
-             throw new RuntimeException("Возникла проблема при чтении или записи файла");
-         }
+        try (InputStream inputStream = file.getInputStream();
+             OutputStream outputStream = Files.newOutputStream(filePath, CREATE_NEW);
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream, 1024);
+             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream, 1024)) {
+            bufferedInputStream.transferTo(bufferedOutputStream);
+        } catch (IOException e) {
+            throw new RuntimeException("Возникла проблема при чтении или записи файла");
+        }
 
-         AvatarStudent avatarStudent = avatarStudentRepository.findByStudentId(id).orElseGet(AvatarStudent::new);
-            avatarStudent.setStudent(student);
-            avatarStudent.setFilePath(filePath.toString());
-            avatarStudent.setFileSize(file.getSize());
-            avatarStudent.setMediaType(file.getContentType());
-            avatarStudent.setPreview(Preview.generateImagePreview(filePath));
-
-            avatarStudentRepository.save(avatarStudent);
+        AvatarStudent avatarStudent = avatarStudentRepository.findByStudentId(id).orElseGet(AvatarStudent::new);
+        avatarStudent.setStudent(student);
+        avatarStudent.setFilePath(filePath.toString());
+        avatarStudent.setFileSize(file.getSize());
+        avatarStudent.setMediaType(file.getContentType());
+        avatarStudent.setPreview(Preview.generateImagePreview(filePath));
+        avatarStudentRepository.save(avatarStudent);
+        studentService.saveAvatar(id, avatarStudent);
 
     }
 
@@ -72,5 +76,16 @@ public class AvatarStudentService {
     public Pair<String, byte[]> getPreview(Long id) {
         AvatarStudent avatarStudent = avatarStudentRepository.findByStudentId(id).orElseThrow(() -> new AvatarNotFoundException(id));
         return Pair.of(avatarStudent.getMediaType(), avatarStudent.getPreview());
+    }
+
+    public List<AvatarStudent> getAvatarPage(int pageNumber, int pageSize){
+        if (pageNumber < 1) {
+            throw new DataEntryError("Номер страницы не должен быть меньше единицы!");
+        }
+        if (pageSize < 1) {
+            throw new DataEntryError("Не должно быть меньше одного элемента на странице!");
+        }
+        PageRequest pageRequest=PageRequest.of(pageNumber-1,pageSize);
+        return avatarStudentRepository.findAll(pageRequest).getContent();
     }
 }
