792846704e81b4420a3883149a1dac62e364a568
NatShiv <happy_fox@bk.ru>
2022-10-29 23:28:08 +0300

    4.4

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,16 +1,22 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import ru.hogwarts.school.entity.EmblemFaculty;
 import ru.hogwarts.school.entity.Student;
 import ru.hogwarts.school.exception.DataEntryError;
 import ru.hogwarts.school.exception.FacultyNotFoundException;
 import ru.hogwarts.school.entity.Faculty;
 import ru.hogwarts.school.repository.FacultyRepository;
 
+import javax.transaction.Transactional;
 import java.util.Collection;
 
 @Service
+@Transactional
 public class FacultyService {
+    private Logger logger = LoggerFactory.getLogger(FacultyService.class);
     private final FacultyRepository facultyRepository;
 
     public FacultyService(FacultyRepository facultyRepository) {
@@ -18,36 +24,58 @@ public class FacultyService {
     }
 
     public Faculty createFaculty(Faculty faculty) {
+        logger.debug("Вызван метод - создть факультет.");
         return facultyRepository.save(faculty);
     }
 
     public Faculty findFaculty(Long id) {
-        return facultyRepository.findById(id).orElseThrow(() -> new FacultyNotFoundException(id));
+        logger.debug("Вызван метод - найти факультет с номером {}.", id);
+        return facultyRepository.findById(id).orElseThrow(() -> {
+            logger.error("Факультет с номером {} не найден", id);
+            return new FacultyNotFoundException(id);
+        });
     }
 
     public Faculty updateFaculty(Long id, Faculty faculty) {
-        facultyRepository.findById(id).orElseThrow(() -> new FacultyNotFoundException(id));
+        logger.debug("Вызван метод - изменить факультет с номером {}.", id);
+     findFaculty(id);
         return facultyRepository.save(faculty);
     }
 
     public Faculty removeFaculty(Long id) {
-        Faculty faculty = facultyRepository.findById(id).orElseThrow(() -> new FacultyNotFoundException(id));
+        logger.debug("Вызван метод - удалить факультет с номером {}.", id);
+        Faculty faculty = findFaculty(id);
         facultyRepository.deleteById(id);
         return faculty;
     }
+
     public Collection<Faculty> findByColor(String color) {
+        logger.debug("Вызван метод - найти факультет по цвету {}.", color);
         if (color.isBlank()) {
+            logger.warn("Пользователь не ввел данные и программа ввела дефолтное пустое значение");
             throw new DataEntryError("Введите цвет факультета.");
         }
-        return facultyRepository.findByColor(color);}
+        return facultyRepository.findByColor(color);
+    }
 
     public Collection<Faculty> findByNameIgnoreCaseOrColorIgnoreCase(String nameOrColor) {
+        logger.debug("Вызван метод - найти факультет по цвету или названию {}.", nameOrColor);
         if (nameOrColor.isBlank()) {
+            logger.warn("Пользователь не ввел данные и программа ввела дефолтное пустое значение");
             throw new DataEntryError("Введите цвет или название факультета.");
         }
-        return facultyRepository.findByNameIgnoreCaseOrColorIgnoreCase(nameOrColor,nameOrColor);
+        return facultyRepository.findByNameIgnoreCaseOrColorIgnoreCase(nameOrColor, nameOrColor);
     }
+
     public Collection<Student> findStudentByFaculty(Long id) {
-             return facultyRepository.findStudents(id);
+        logger.debug("Вызван метод - найти всех студентов факультетас номером {}.", id);
+        return facultyRepository.findStudents(id);
+    }
+
+    public void saveEmblem(Long id, EmblemFaculty emblemFaculty) {
+        logger.debug("Вызван метод - прикрепить эмблему факультету с номером {}.", id);
+        Faculty faculty = findFaculty(id);
+        faculty.emblemFacultySet(emblemFaculty);
+        facultyRepository.save(faculty);
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,15 +1,15 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.entity.AvatarStudent;
 import ru.hogwarts.school.entity.Faculty;
 import ru.hogwarts.school.exception.DataEntryError;
-import ru.hogwarts.school.exception.FacultyNotFoundException;
 import ru.hogwarts.school.exception.StudentNotFoundException;
 import ru.hogwarts.school.entity.Student;
-import ru.hogwarts.school.repository.FacultyRepository;
 import ru.hogwarts.school.repository.StudentRepository;
 import ru.hogwarts.school.validator.Validator;
 
@@ -22,50 +22,62 @@ import java.util.Collection;
 @Service
 @Transactional
 public class StudentService {
+    private static Logger logger= LoggerFactory.getLogger(StudentService.class);
     private final StudentRepository studentRepository;
-    private final FacultyRepository facultyRepository;
+    private final FacultyService facultyService;
 
-    public StudentService(StudentRepository studentRepository, FacultyRepository facultyRepository) {
+    public StudentService(StudentRepository studentRepository,FacultyService facultyService) {
         this.studentRepository = studentRepository;
-        this.facultyRepository = facultyRepository;
+        this.facultyService = facultyService;
     }
 
       public Student createStudent(Student student) {
-        facultyRepository.findById(student.getFaculty().getId()).orElseThrow(() -> new FacultyNotFoundException(student.getFaculty().getId()));
+          logger.debug("Вызван метод - создать студента.");
+      facultyService.findFaculty(student.getFaculty().getId());
         return studentRepository.save(student);
     }
 
-    public Student findStudent(Long id) {
-        return studentRepository.findById(id).orElseThrow(() -> new StudentNotFoundException(id));
+    public Student findStudent(Long id) {  logger.debug("Вызван метод - найти студента с номером {}.",id);
+        return studentRepository.findById(id).orElseThrow(() -> {
+            logger.error("Студент с номером {} не найден", id);
+            return new StudentNotFoundException(id);
+        });
     }
 
     public Student updateStudent(Long id, Student student) {
-        facultyRepository.findById(student.getFaculty().getId()).orElseThrow(() -> new FacultyNotFoundException(student.getFaculty().getId()));
-        studentRepository.findById(id).orElseThrow(() -> new StudentNotFoundException(id));
+        logger.debug("Вызван метод - изменить студента с номером {}.",id);
+        facultyService.findFaculty(student.getFaculty().getId());
+      findStudent(id);
         return studentRepository.save(student);
     }
 
     public Student removeStudent(Long id) {
-        Student student = studentRepository.findById(id).orElseThrow(() -> new StudentNotFoundException(id));
+        logger.debug("Вызван метод - удалить студента с номером {}.",id);
+        Student student = findStudent(id);
         studentRepository.deleteById(id);
         return student;
     }
 
     public Collection<Student> findStudentByAge(int age) {
+        logger.debug("Вызван метод - найти студентов с возрастом {}.",age);
         return studentRepository.findByAge(Validator.validateNumber(age));
     }
 
     public Faculty findFacultyByStudent(Long id) {
+        logger.debug("Вызван метод - вывести факультет студента с номером {}.",id);
         Student student = studentRepository.findById(id).orElseThrow(() -> new StudentNotFoundException(id));
         Faculty faculty = student.getFaculty();
         if (faculty == null) {
+            logger.error("Студент с номером {} не распределен на факультет.",id);
             throw new DataEntryError("Студент не распределен на факультет.");
         }
         return faculty;
     }
 
     public Collection<Student> findStudentBetweenAge(int min, int max) {
+        logger.debug("Вызван метод - найти студентов возраст которых лежит в пределах{} {}.",min,max);
         if (max < min) {
+            logger.error("Минимальное значение возраста не может быть больше максимального.");
             throw new DataEntryError("Минимальное значение возраста не может быть больше максимального.");
         }
         return studentRepository.findByAgeBetween(Validator.validateNumber(min),
@@ -73,29 +85,35 @@ public class StudentService {
     }
 
     public Pair<String, byte[]> getAvatar(Long id) {
+        logger.debug("Вызван метод - показать аватар студента с номером {}.",id);
         Student student = studentRepository.findById(id).orElseThrow(() -> new StudentNotFoundException(id));
         AvatarStudent avatarStudent = student.avatarGet();
         try {
             return Pair.of(avatarStudent.getMediaType(), Files.readAllBytes(Paths.get(avatarStudent.getFilePath())));
         } catch (IOException e) {
+            logger.error("Возникла проблема при чтении файла");
             throw new RuntimeException("Возникла проблема при чтении файла");
         }
     }
     public void saveAvatar(Long id,AvatarStudent avatarStudent){
-        Student student = studentRepository.findById(id).orElseThrow(() -> new StudentNotFoundException(id));
+        logger.debug("Вызван метод - сохранить аватар студенту с номером {}.",id);
+        Student student = findStudent(id);
         student.avatarSet(avatarStudent);
         studentRepository.save(student);
     }
 
     public int getTotalStudent() {
+        logger.debug("Вызван метод - показать число всех студентов школы.");
         return studentRepository.totalStudents();
     }
 
     public int getAverageAgeStudents() {
+        logger.debug("Вызван метод - показать средний возраст студентов." );
         return studentRepository.averageAge();
     }
 
     public Collection<Student> getFiveEndStudents(){
+        logger.debug("Вызван метод - показать 5 последних студентов в списке.");
         PageRequest pageRequest=PageRequest.of(0,5);
         return studentRepository.getFiveEndStudents(pageRequest);
     }
